ca6eef475101e449ee0c80dc939657fa1494803d
LM Cool J <lmjma13@gmail.com>
2025-07-23 20:31:10 -0700

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 function checkCode(){
   var code = document.getElementById("Code").value;
-  if (code == "dev_wufes") {
+  if (code === "dev_wufes" || code === "dev_kookoo") {
     window.location.href = "rookhub.html";
   } else if (code !== "") {
     alert("Invalid code! Please try again.");
